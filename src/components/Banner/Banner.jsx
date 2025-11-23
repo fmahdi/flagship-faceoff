b80b2b4fc8276876325519ea200f39ffa3e60993
Fahim MIS <fahim.wakrah@gmail.com>
2025-11-23 13:15:35 +0600

    added components to Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import bannerImage from "../../assets/banner.png"
 
 const Banner = () => {
     return (
-        <div>
-            <h1>I am a pretty Hero section</h1>
+        <div className="w-full mx-auto md:max-w-md">
+            <img src={bannerImage} alt="This is banner habibi" />
+            <div>
+                <h1 className="font-thin text-7xl text-gray-900">Browse, Search, View, Buy</h1>
+                <p className="text-gray-600">Best place to browse, search, view details and purchase of top flagship phones
+                    of the current time - FlagshipFaceOff</p>
+                <form className="flex flex-col md:flex-row justify-center items-center mb-4">
+                    <input type="text" placeholder="Search Phone by Name" />
+                    <button>Search</button>
+                </form>
+            </div>
         </div>
     );
 };
