e045296adc6af5686f9c357881f251b1a2d6a093
DevPentakill <fmahdi.bd@gmail.com>
2025-12-03 00:43:54 +0600

    added useeffect changes

--- src/components/PhonesContainer/PhonesContainer.jsx
+++ src/components/PhonesContainer/PhonesContainer.jsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import PhoneCard from "../PhoneCard/PhoneCard";
 
 const PhonesContainer = ({ phones }) => {
+    console.log(phones);
     const { displayPhones, setDisplayPhones } = useState([]);
     const { showAll, setShowAll } = useState(false);
     useEffect(() => {
         if (showAll) {
             setDisplayPhones(phones);
-        }else{
-            setDisplayPhones(phones.slice(0,6));
+        } else {
+            setDisplayPhones(phones.slice(0, 6));
         }
-    }, [setDisplayPhones, showAll])
+    }, [phones, showAll])
 
     return (
         <div className="space-y-7">
@@ -21,7 +22,7 @@ const PhonesContainer = ({ phones }) => {
                 }
             </div>
             <div className="mb-6 text-center">
-                <a onClick={() => { prev => !setShowAll(prev) }} className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
+                <a onClick={() => setShowAll(prev => !prev)} className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
                     <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
                     <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
                     <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
