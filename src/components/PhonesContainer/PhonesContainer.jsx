ea0cac14fd4a6210f6efcfc607ce72dc0c73219f
Fahim MIS <fahim.wakrah@gmail.com>
2025-12-03 11:29:22 +0600

    fixed typo of array destructuring

--- src/components/PhonesContainer/PhonesContainer.jsx
+++ src/components/PhonesContainer/PhonesContainer.jsx
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 import PhoneCard from "../PhoneCard/PhoneCard";
 
 const PhonesContainer = ({ phones }) => {
-    console.log(phones);
-    const { displayPhones, setDisplayPhones } = useState([]);
-    const { showAll, setShowAll } = useState(false);
+    const  [displayPhones, setDisplayPhones] = useState([]);
+    const [showAll, setShowAll] = useState(false);
     useEffect(() => {
         if (showAll) {
             setDisplayPhones(phones);
         } else {
             setDisplayPhones(phones.slice(0, 6));
         }
-    }, [phones, showAll])
+    }, [phones, showAll]);
 
     return (
         <div className="space-y-7">
@@ -29,9 +28,7 @@ const PhonesContainer = ({ phones }) => {
                     <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
                     <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
                     <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
-
-                        Show All
-
+                    {showAll?"Show less":"Show all"}
                     </span>
                 </a>
             </div>
