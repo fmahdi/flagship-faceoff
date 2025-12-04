bbd00c2bcff776201133ec9b7ac6990c5f3686f9
DevPentakill <fmahdi.bd@gmail.com>
2025-12-05 00:14:28 +0600

    created UI element Button for resuability

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import bannerImage from "../../assets/banner.png"
+import bannerImage from "../../assets/banner.png";
+import Buttons from "../UI/Buttons";
 
 const Banner = ({ handleSearch }) => {
     const [searchText, setSearchText] = useState("");
@@ -25,11 +26,8 @@ const Banner = ({ handleSearch }) => {
                     <input value={searchText} onChange={(e) => setSearchText(e.target.value)} className="bg-white border-grey-300 rounded md:mr-2 shadow-md w-2/3 h-12 px-4 mb-3 focus:outline-none focus:shadow-outline" type="text" placeholder="Search Phone by Name" />
 
                     {/* stylized button */}
-                    <button type="submit" className="relative inline-block px-4 py-2 font-medium group">
-                        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                        <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                        <span className="relative text-black group-hover:text-white">Search</span>
-                    </button>
+                    <Buttons type="submit" label="Search"></Buttons>
+
                 </form>
             </div>
         </div>
--- src/components/PhoneDetails/PhoneDetails.jsx
+++ src/components/PhoneDetails/PhoneDetails.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData, useParams } from "react-router";
 import { MdBookmarkAdd, MdOutlineAddShoppingCart } from "react-icons/md";
+import Buttons from "../UI/Buttons";
 
 const PhoneDetails = () => {
     const data = useLoaderData();
@@ -8,12 +9,12 @@ const PhoneDetails = () => {
     const { image, name, description, brand, price, storage, camera_info } = currentPhone;
     return (
         <div className="border-2 p-10">
-            <img className="w-auto mx-auto" src={image} alt="current Phone" />
+            <img className="w-full md:w-auto mx-auto mb-8" src={image} alt="current Phone" />
             <div className="flex justify-between">
                 <h1 className="text-5xl font-thin">{name}</h1>
-                <div className="flex justify-between">
-                    <MdBookmarkAdd />
-                    <MdOutlineAddShoppingCart />
+                <div className="flex justify-between gap-4">
+                    <Buttons label={<MdBookmarkAdd />}></Buttons>
+                    <Buttons label={<MdOutlineAddShoppingCart />}></Buttons>
                 </div>
             </div>
             <p>There are many models and companies available for cell phones. This is optimal</p>
