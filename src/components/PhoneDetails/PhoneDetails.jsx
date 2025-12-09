3120d28181daba63af27e93acf6ec47aa4e4b900
DevPentakill <fmahdi.bd@gmail.com>
2025-12-09 23:11:30 +0600

    added the price and storage details in individual phone list

--- src/components/PhoneDetails/PhoneDetails.jsx
+++ src/components/PhoneDetails/PhoneDetails.jsx
@@ -8,16 +8,66 @@ const PhoneDetails = () => {
     const currentPhone = data.find(elem => elem.id === parseInt(id));
     const { image, name, description, brand, price, storage, camera_info } = currentPhone;
     return (
-        <div className="border-2 p-10">
+        <div className="p-10">
             <img className="w-full md:w-auto mx-auto mb-8" src={image} alt="current Phone" />
-            <div className="flex justify-between">
+            <div className="flex justify-between mb-5">
                 <h1 className="text-5xl font-thin">{name}</h1>
                 <div className="flex justify-between gap-4">
                     <Buttons label={<MdBookmarkAdd />}></Buttons>
                     <Buttons label={<MdOutlineAddShoppingCart />}></Buttons>
                 </div>
             </div>
-            <p>There are many models and companies available for cell phones. This is optimal</p>
+            <p className="text-4xl font-thin">Product Details:</p>
+            <div>
+
+                <div className="mt-6 border-t border-gray-100">
+                    <dl className="divide-y divide-gray-100">
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">Brand name</dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{brand}</dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">Description</dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">{description}</dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">Storage</dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {
+                                    storage.map((elem, index) => <div key={index} className="badge badge-soft badge-primary mr-3">{elem}</div>)
+                                }
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">Pricing</dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {
+                                    Object.entries(price).map(([key, value], index) => {
+
+                                        return <div key={index}>
+                                            <div className="badge badge-outline badge-primary mr-2">{key}</div>
+                                            <div className="badge badge-primary">{value}</div>
+                                        </div>
+
+                                    }
+
+                                    )
+                                }
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">About</dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+                                qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
+                                pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+
+
         </div>
     );
 };
