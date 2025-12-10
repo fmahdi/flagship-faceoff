cb7aff237be657aa5648075b848b2634d373f943
DevPentakill <fmahdi.bd@gmail.com>
2025-12-11 01:15:54 +0600

    created the favorite list with phone cards

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getFav } from '../../utils';
+import PhoneCard from '../../components/PhoneCard/PhoneCard';
 
 const Favorites = () => {
     const [displayPhones, setDisplayPhones] = useState([]);
 
+    useEffect(() => {
+
+        const favoriteList = getFav();
+        console.log(favoriteList);
+        setDisplayPhones(favoriteList);
+    }, []);
+
     return (
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-3'>
             {
                 displayPhones.map((elem) => <PhoneCard key={elem.id} phone={elem} deletable={true}></PhoneCard>)
             }
